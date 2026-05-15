01749d75e03f01c966ee6666dbb729fa0caab67b
JoelitoCS <canosanchezjoel@fpllefia.com>
2026-05-15 13:05:42 +0200

    añadimos animacion de fondo y añado boton para descargar curriculum, complemento bien el about me con buen formato

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'DevJoel — Portfolio';
+export const SITE_DESCRIPTION =
+	'Portfolio de Joel Cano Sánchez, desarrollador full-stack junior. Proyectos web con React, Node.js, Laravel y más.';
