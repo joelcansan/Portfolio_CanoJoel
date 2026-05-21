8d8fe786a7c1fba3124f298fbd8a953f4aa4c985
JoelitoCS <canosanchezjoel@fpllefia.com>
2026-05-21 13:03:52 +0200

    todo perfecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true
+  },
+  preview: {
+    host: true,
+    allowedHosts: ['portfolio-canojoel.onrender.com']
+  }
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
 
