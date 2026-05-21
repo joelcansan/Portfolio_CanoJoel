fe778558366cc4f903490a8dcf81c9abaf3717d3
JoelitoCS <canosanchezjoel@fpllefia.com>
2026-05-21 12:39:21 +0200

    Cambio en el title de la pagina

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'DevJoel — Portfolio';
+export const SITE_TITLE = 'JCS — Portfolio';
 export const SITE_DESCRIPTION =
 	'Portfolio de Joel Cano Sánchez, desarrollador full-stack junior. Proyectos web con React, Node.js, Laravel y más.';
